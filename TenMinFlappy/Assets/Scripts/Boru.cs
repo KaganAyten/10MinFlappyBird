d43db2cde73754f12873ba5c7c3b7e9a4e3abec2
Kagan Ayten <kaan.aytn@hotmail.com>
2021-01-24 17:37:54 +0300

    10 Min Game

---
+++ TenMinFlappy/Assets/Scripts/Boru.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boru : MonoBehaviour
+{
+    //-2.1 to 4.4
+    [SerializeField]
+    float hareketHizi;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -2.1f)
+        {
+            transform.position = new Vector3(4.4f, transform.position.y, transform.position.z);
+        }
+        gameObject.transform.Translate(-hareketHizi * Time.deltaTime, 0, 0);
+
+    }
+}
---
+++ TenMinFlappy/Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField]
+    float forceSpeed;
+
+    public bool isDead;
+    public GameObject uyari;
+    public Text skorText;
+    int skor;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        isDead = false;
+        uyari.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        skorText.text = skor.ToString();
+        if (!isDead)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                Zipla();
+            }
+        }
+        else
+        {
+            uyari.SetActive(true);
+        }
+    }
+    void Zipla()
+    {
+        rb.AddForce(Vector2.up * forceSpeed, ForceMode2D.Impulse);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "boru" || collision.gameObject.tag == "zemin")
+        {
+            isDead = true;
+        }
+        if (collision.gameObject.tag == "skorArea")
+        {
+            skor++;
+        }
+    }
+}
